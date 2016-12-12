e25005085a61b82ac1ab6174a642e91fba41e8ac
Vini Santana <vini@administrators-MacBook-Pro.local>
2016-12-11 18:55:51 -0800

    Done with http://www.adequatelygood.com/JavaScript-Module-Pattern-In-Depth.html

--- module-fun.js
+++ module-fun.js
@@ -37,7 +37,8 @@ var MODULE = (function () {
 
   my.moduleProperty = 1;
   my.moduleMethod = function () {
-    // ...        
+    // ...     
+        console.log("Original my.moduleMethod")
   };
 
   return my;
@@ -72,3 +73,84 @@ var MODULE = (function (my) {
 }(MODULE || {}));
 
 console.log(MODULE);
+
+
+// Tight Augmentation
+
+var MODULE = (function (my) {
+    console.log("Tight Augmentation");
+  var old_moduleMethod = my.moduleMethod;
+
+    my.old =  old_moduleMethod;
+  my.moduleMethod = function () {
+    // method override, has access to old through old_moduleMethod...
+        console.log("New my.moduleMethod")
+  };
+
+  return my;
+}(MODULE));
+
+console.log(MODULE);
+
+// Cloning and Inheritance
+
+var MODULE_TWO_AKA_THE_CLONE = (function (old) {
+    console.log("Cloning and Inheritance");
+  var my = {},
+    key;
+
+  for (key in old) {
+    if (old.hasOwnProperty(key)) {
+      my[key] = old[key];
+    }
+  }
+
+  var super_moduleMethod = old.moduleMethod;
+  my.moduleMethod = function () {
+    // override method on the clone, access to super through super_moduleMethod
+  };
+
+  return my;
+}(MODULE));
+
+console.log("MODULE", MODULE);
+console.log("MODULE_TWO_AKA_THE_CLONE", MODULE_TWO_AKA_THE_CLONE);
+
+
+
+// Cross-File Private State
+
+var MODULE = (function (my) {
+    console.log("Cross-File Private State");
+  var _private = my._private = my._private || {},
+    _seal = my._seal = my._seal || function () {
+      delete my._private;
+      delete my._seal;
+      delete my._unseal;
+    },
+    _unseal = my._unseal = my._unseal || function () {
+      my._private = _private;
+      my._seal = _seal;
+      my._unseal = _unseal;
+    };
+
+  // permanent access to _private, _seal, and _unseal
+
+  return my;
+}(MODULE || {}));
+
+console.log(MODULE);
+
+
+// Sub-modules
+
+MODULE.sub = (function () {
+    console.log("Sub-modules");
+  var my = {};
+  // ...
+
+  return my;
+}());
+
+console.log('MODULE', MODULE);
+console.log('MODULE.sub', MODULE.sub);
